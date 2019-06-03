51ea2494d611a6cb6392b6ade162074d1b64438e
Anh Nguyen <nvtienanh@gmail.com>
2019-06-03 14:21:42 +0700

    Update image files

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-POSTGRES_TAG := 9.6-hive2.3.4
+POSTGRES_TAG := 9.6
 build:
 	docker build -t nvtienanh/postgresql:$(POSTGRES_TAG) ./
 push:
--- init-hive-db.sh
+++ init-hive-db.sh
@@ -3,19 +3,11 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
   CREATE USER hue WITH PASSWORD 'hue';
-  CREATE DATABASE metastore;
-  GRANT ALL PRIVILEGES ON DATABASE metastore TO hue;
+  CREATE DATABASE hue;
+  GRANT ALL PRIVILEGES ON DATABASE hue TO hue;
 
-  \c metastore
-
-  \i /hive/hive-schema-2.3.0.postgres.sql
-  \i /hive/hive-txn-schema-2.3.0.postgres.sql
-
-  \pset tuples_only
-  \o /tmp/grant-privs
 SELECT 'GRANT SELECT,INSERT,UPDATE,DELETE ON "' || schemaname || '"."' || tablename || '" TO hue ;'
 FROM pg_tables
 WHERE tableowner = CURRENT_USER and schemaname = 'public';
-  \o
-  \i /tmp/grant-privs
+
 EOSQL
